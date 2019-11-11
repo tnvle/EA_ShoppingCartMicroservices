df0820b7ef84599a7987819fd8866e1327473394
Islam Ahmad <islam.ahmad@gmail.com>
2019-11-11 09:45:44 -0600

    updated application.properties for inventory service

--- inventory-service/src/main/resources/import.sql
+++ inventory-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into inventory(product_code, quantity) VALUES('P001', 100)
-insert into inventory(product_code, quantity) VALUES('P002', 90)
-insert into inventory(product_code, quantity) VALUES('P003', 80)
+insert into inventory(product_code, quantity) VALUES('P001', 100);
+insert into inventory(product_code, quantity) VALUES('P002', 90);
+insert into inventory(product_code, quantity) VALUES('P003', 80);
