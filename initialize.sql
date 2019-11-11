9fbc803208714afe62418d2d055710f8f48fe777
Islam Ahmad <islam.ahmad@gmail.com>
2019-11-11 07:24:21 -0600

    added script to initialize the db schemas

---
+++ initialize.sql
@@ -0,0 +1,9 @@
+create schema db_account;
+create schema db_authenticate;
+create schema db_bankaccount;
+create schema db_catalog;
+create schema db_creditcard;
+create schema db_inventory;
+create schema db_order;
+create schema db_payment;
+create schema db_shipping;
